90460d27f0a3fc5b4240b79f8cfe70d1e2343cb3
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2023-09-09 15:53:09 -0400

    Fill out owner dashboard project

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,12 @@
 // custom typefaces
-import "@fontsource-variable/montserrat"
-import "@fontsource/merriweather"
+import '@fontsource-variable/montserrat'
+import '@fontsource/merriweather'
+
 // normalize CSS across browsers
-import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
+import './src/normalize.css'
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import 'prismjs/themes/prism.css'
+
+// custom CSS styles
+import './src/style.css'
