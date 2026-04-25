356612e73218ad50de6cc4c489a99b204faa2b47
Tucker Hawkinson <public@hawkinson.dev>
2026-04-25 17:51:05 -0400

    uninstall wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   output: "static",
 
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: cloudflare(),
 });
